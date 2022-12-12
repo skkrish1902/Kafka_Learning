7457388f08bf91052813bbf318522377a544df5f
skkrish1902 <43465813+skkrish1902@users.noreply.github.com>
2022-12-12 11:19:59 +0530

    initial commit

---
+++ Learning/src/test/java/com/kafka/Learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.Learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
